e199a9f95e4a92efad30f684252217d35c1dd000
Tom Buckley <tbuckley89@gmail.com>
2015-07-11 20:27:37 -0700

    Updated helloworld example

--- examples/helloworld.go
+++ examples/helloworld.go
@@ -2,37 +2,43 @@ package main
 
 import (
 	"fmt"
-	"github.com/mikeflynn/go-alexa/skillserver"
 	"net/http"
+
+	"github.com/tbuckley/go-alexa"
 )
 
-var Applications = map[string]interface{}{
-	"/echo/helloworld": skillserver.EchoApplication{ // Route
-		AppID:   "xxxxxxxx",     // Echo App ID from Amazon Dashboard
-		Handler: EchoHelloWorld, // Handler Func
-	},
-	"/": skillserver.StdApplication{
-		Methods: "GET",
-		Handler: HomePage,
-	},
-}
+var (
+	myskill *alexa.Skill
+)
 
 func main() {
-	skillserver.Run(Applications, "3000")
-}
+	myskill = alexa.New("AMAZON APP ID") // Create a new skill with your app id
 
-func EchoHelloWorld(w http.ResponseWriter, r *http.Request) {
-	echoReq := skillserver.GetEchoRequest(r)
-
-	if echoReq.GetRequestType() == "IntentRequest" || echoReq.GetRequestType() == "LaunchRequest" {
-		echoResp := skillserver.NewEchoResponse().OutputSpeech("Hello world from my new Echo test app!").Card("Hello World", "This is a test card.")
-
-		json, _ := echoResp.String()
-		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-		w.Write(json)
-	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomePage)
+	mux.HandleFunc("/echo/helloworld", EchoHelloWorld)
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page!")
+}
+
+func EchoHelloWorld(w http.ResponseWriter, r *http.Request) {
+	// Use HandlerFuncWithNext to wrap existing functions or with a framework
+	// like Negroni
+	myskill.HandlerFuncWithNext(w, r, func(w http.ResponseWriter, r *http.Request) {
+		// Get the Echo request object
+		echoReq := alexa.GetEchoRequest(r)
+
+		if echoReq.GetRequestType() == "IntentRequest" || echoReq.GetRequestType() == "LaunchRequest" {
+			// Create a response
+			echoResp := alexa.NewResponse()
+			echoResp.OutputSpeech("Hello world from my new Echo test app!")
+			echoResp.Card("Hello World", "This is a test card.")
+
+			json, _ := echoResp.ToJSON()
+			w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+			w.Write(json)
+		}
+	})
 }
